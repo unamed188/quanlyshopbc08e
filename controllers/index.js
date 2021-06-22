39404a487a8ebcbb0099ec0d04c15a4cb11708f5
dev B <hoang.bao9220@gmail.com>
2021-06-22 18:21:53 +0700

    version 2_vietChucNangRenderProduct

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,29 @@
 console.log('hello cybersoft')
+
+
+var arrProduct = [
+    {id:1, name:'iphone x',price: 1000, img: 'http://picsum.photos/id/1/200/200'},
+    {id:2, name:'iphone xs',price: 2000, img: 'http://picsum.photos/id/2/200/200'},
+    {id:3, name:'iphone xs max',price: 3000, img: 'http://picsum.photos/id/3/200/200'},
+]
+
+function renderProduct (){
+    var content = '';
+    for(var i=0;i<arrProduct.length;i++) {
+        let product = arrProduct[i]
+        content += `
+        <div class="col-4 mt-2">
+            <div class="card">
+                <img src="${product.img}" alt="..."/>
+                <div class="card-body">
+                    <p>${product.price}</p>
+                    <button>Mua hàng</button>
+                </div>
+            </div>
+        </div>        
+        `
+    }
+
+    document.querySelector('#arrProduct').innerHTML = content
+}
+renderProduct();
